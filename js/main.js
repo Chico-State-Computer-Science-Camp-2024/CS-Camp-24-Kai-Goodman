07f01c7d230ae87ccdb1c5e118a5e4b8fa1e9e6f
KaiTheProgrammer <kgoodman1@csuchico.edu>
2024-07-02 22:51:34 +0000

    done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
-    });
-});
+let age = prompt("How old are you?");
+age = Number(age);
+
+if (age < 18) {
+    console.log("Welcome, young apprentice!");
+} else if (age >= 18) {
+    console.log("Welcome, ancient One!");
+}  else {
+    console.log("Invalid input");
+}
