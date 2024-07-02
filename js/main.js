cb4eb44af3b9cd85adea2b0295901cebab32f2e1
KaiTheProgrammer <kgoodman1@csuchico.edu>
2024-07-02 18:21:38 +0000

    ghj

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
     $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
+        window.alert("I have a lifespan of nearly 20 years! I eat crabs, shrimp, lobsters, fish, mollusks, oysters, and snails. My species have been around for roughly 400 million years!!");
     });
 });
